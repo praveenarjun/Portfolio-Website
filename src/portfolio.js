7af9a4a4d1aff33f3cc5d69ab6ace6b32f5d8d48
Sree Praveen Challa <130534146+praveenarjun@users.noreply.github.com>
2026-01-01 14:39:30 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://docs.google.com/document/d/1roWBKar-4dfiyteH2BdsW8aSuR5YZUqjIvwLOB_r55I/edit?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1jEVk4fCwgPQ0V2QDbCjA5mhFZuG1glVv/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
